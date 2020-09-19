1351dcd34633f131717683db8712b4673063ec70
apa yu <rx836@hotmail.com>
2020-09-19 16:18:58 +0800

    for test

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,23 @@
+require 'byebug'
+
 class TasksController < Mavericks::Controller
   def index
+    @tasks = FileModel.all
+  end
 
+  def show
+    @task = FileModel.find(params['id'])
+    @user_agent = request.user_agent
+  end
+
+  def create
+    attrs = {
+      title: '週末跟朋友聚餐',
+      content: '喝酒喝到飽'
+    }
+
+    FileModel.create(attrs)
+    @tasks = FileModel.all
+    render 'index'
   end
 end
---
+++ sqlite_test.rb
@@ -0,0 +1,61 @@
+require 'sqlite3'
+require 'mavericks/data_record'
+require 'mavericks/sqlite_model'
+
+Mavericks::DataRecord::Base.establish_connection(
+  adapter: 'postgresql',
+  database: 'my_db'
+)
+
+class Task < Mavericks::Model::SQLite
+end
+
+# class Task < Mavericks::Model::SQLite
+#   Task.schema.keys.each do |attr|
+#     define_method attr do
+#       self[attr.to_s]
+#     end
+#
+#     define_method "#{attr}=" do |arg|
+#       self[attr.to_s] = arg
+#     end
+#   end
+# end
+
+# puts "Count: #{Task.count}"
+# Task.all.each { |task| puts task['title'] }
+# puts Task.find(1)['title']
+
+# task = Task.create('title': '鐵人30', 'content': '每天一篇文章')
+# puts task['title']
+# task['title'] = 'IT邦幫忙鐵人賽'
+# task.save!
+# Task.all.each { |task| puts task['title'] }
+
+# task = Task.new('title': '鐵人30', 'content': '每天一篇文章')
+# task.save!
+# Task.all.each { |task| puts task['title'] }
+
+# puts task['title']
+# puts task.title
+
+# class Amo
+#   define_method :hola do |my_arg|
+#     "Hi! #{my_arg}"
+#   end
+# end
+#
+# puts Amo.new.hola('apa')
+
+# 給 method_missing 的測試
+# task = Task.new('title': '鐵人30', 'content': '每天一篇文章')
+# task.save!
+# Task.all.each { |task| puts task.id }
+# # Task.all.each { |task| puts task.title }
+# # Task.all.each { |task| puts task.content }
+# task2 = Task.find(1)
+# task2.title = '鐵人40'
+# task2.save
+# task3 = Task.find(1)
+# puts task3.title
+#
